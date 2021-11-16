27d81a002a0a8b9edd42c8a7c3cb1bfc6e57b364
zanyxdev <23155424+ZanyXDev@users.noreply.github.com>
2021-11-16 19:46:42 +0300

    add model to qml

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,7 @@
 #include <QtQml/QQmlContext>
 #include <QtQuickControls2/QQuickStyle>
 
+#include "tilemodel.h"
 
 int main(int argc, char *argv[])
 {
@@ -42,13 +43,30 @@ int main(int argc, char *argv[])
     app.installTranslator(&myappTranslator);
 
     QQuickStyle::setStyle(QStringLiteral("Universal"));
+#ifdef QT_DEBUG
+    const QUrl url(QStringLiteral("qrc:/res/qml/TestWindow.qml"));
+#else
     const QUrl url(QStringLiteral("qrc:/res/qml/main.qml"));
+#endif
+    TileModel tileModel;
+#ifdef QT_DEBUG
+    for (int i=0;i <25;i++){
+        tileModel.append({false,false,true,QString("%1").arg(i)});
+    }
+    qDebug() << tileModel.rowCount(QModelIndex());
+#endif
+    /**
+     * @brief qmlRegisterType<TileModel>("io.github.zanyxdev", 1, 0, "TileModel");
+     * register the type TileModel under the url "io.github.zanyxdev"
+     * in version 1.0 under the name "TileModel"
+     */
+    //qmlRegisterType<TileModel>("io.github.zanyxdev", 1, 0, "TileModel");
 
     //qmlRegisterType<Tile>("gameCore", 1, 0, "Tile");
     QQmlApplicationEngine engine;
 
     QQmlContext* context = engine.rootContext();
-    //context->setContextProperty("gameData", &gameData);
+    context->setContextProperty("tileModel", &tileModel);
 
     QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
                      &app, [url](QObject *obj, const QUrl &objUrl) {
